d9d0a0afb5cf9d2d63062a0542812e46d688b67e
cho_git <whqjarn111@gmail.com>
2023-04-26 19:34:49 +0900

    push 안된 것 재 push

--- MemberAndBoard/src/main/java/handler/loginHandler/LoginPageHandler.java
+++ MemberAndBoard/src/main/java/handler/loginHandler/LoginPageHandler.java
@@ -2,6 +2,7 @@ package handler.loginHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import handler.CommandHandler;
 
@@ -9,6 +10,17 @@ public class LoginPageHandler implements CommandHandler {
 
 	@Override
 	public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		HttpSession session = request.getSession();
+		String id = (String)session.getAttribute("id");
+		int result;
+		if(id == null) {
+			result = 0;
+			request.setAttribute("result", result);
+		}
+		else {
+			result = 1;
+			request.setAttribute("result", result);
+		}
 		
 		
 		return "/member/loginPage.jsp";
